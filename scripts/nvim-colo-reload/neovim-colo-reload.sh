5c930101dbb5e7d28f1d8213dfa1cef44f3a5279
eylles <ed.ylles1997@gmail.com>
2025-03-27 23:50:23 -0600

    add nvim_send function

--- scripts/nvim-colo-reload/neovim-colo-reload.sh
+++ scripts/nvim-colo-reload/neovim-colo-reload.sh
@@ -9,8 +9,12 @@ statusbar=""
 config_dir="${XDG_CONFIG_HOME:-~/.config}/nvim-colo-reload"
 config_file="${config_dir}/configrc"
 
+nvim_send () {
+    nvim --server "$pipe" --remote-send ":${1}<CR>"
+}
+
 rel_colo () {
-    nvim --server "$pipe" --remote-send ":colorscheme ${colorscheme}<CR>"
+    nvim_send "colorscheme ${colorscheme}"
 }
 
 rel_bar () {
@@ -21,7 +25,7 @@ rel_bar () {
                 send_com="lua require'lualine'.setup{options={theme='${colorscheme}'}}"
                 ;;
         esac
-        nvim --server "$pipe" --remote-send ":$send_com<CR>"
+        nvim_send ":$send_com<CR>"
     fi
 }
 
