a2adf1a987de4635cea92a294c60e1eb897d3046
eylles <ed.ylles1997@gmail.com>
2025-03-28 15:59:24 -0600

    nvim-wrap script

---
+++ scripts/nvim-colo-reload/nvim-wrap.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+
+myname="${0##*/}"
+
+# you can change this on config to the correct path on your system
+nv_bin="/usr/bin/nvim"
+
+pipe="/tmp/nvim.pipe"
+
+config_dir="${XDG_CONFIG_HOME:-${HOME}/.config}/nvim-wrap"
+config_file="${config_dir}/configrc"
+
+# loading the config here means the user can overwrite any of the functions
+if [ -f "$config_file" ]; then
+    . "$config_file"
+else
+    if [ ! -d "$config_dir" ]; then
+        mkdir -p "$config_dir"
+    fi
+    cat << __HEREDOC__ >> "$config_file"
+# vim: ft=sh
+# ${myname} config file
+
+# neovim binary
+nv_bin="${nv_bin}"
+
+pipe="${pipe}"
+__HEREDOC__
+fi
+
+exec $nv_bin --listen "$pipe" "$@"
